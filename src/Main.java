e5e037ffc60cb35cfbef54d2561e780e46fe71e8
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-03-14 22:23:24 +0100

    Event management

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/dk/easv/GUI/View/LoginScreen.fxml")));
+        Scene scene = new Scene(parent);
+        primaryStage.setTitle("Login");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
