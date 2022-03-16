982dd6232d3c5f9145943ceb5dd0088e9bb40701
MathiasKrarup <math249e@easv365.dk>
2022-03-16 03:07:27 +0100

    adminView fxml forslag

--- src/dk/easv/GUI/Controller/LoginScreen.java
+++ src/dk/easv/GUI/Controller/LoginScreen.java
@@ -1,4 +1,13 @@
 package dk.easv.GUI.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginScreen {
+    @FXML
+    private PasswordField passwordTxtField;
+    @FXML
+    private TextField emailTxtField;
 }
