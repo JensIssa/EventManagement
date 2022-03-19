bde36f1d20d0a3a64fa052ce931bb354a4fe8f94
MathiasKrarup <math249e@easv365.dk>
2022-03-19 19:37:44 +0100

    Edit+Add eventmanager GUI

---
+++ src/dk/easv/GUI/Controller/AddEventmanagerController.java
@@ -0,0 +1,20 @@
+package dk.easv.GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddEventmanagerController {
+    @FXML
+    private TextField nameTxtField;
+    @FXML
+    private TextField emailTxtField;
+    @FXML
+    private TextField eventTxtField;
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+}
---
+++ src/dk/easv/GUI/Controller/EditEventmanagerController.java
@@ -0,0 +1,16 @@
+package dk.easv.GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class EditEventmanagerController {
+    private TextField emailTxtField;
+    private TextField nameTxtField;
+    private TextField eventTxtField;
+
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+}
