e734160d7348e78730c4606f35c9041a1ed67b7a
MathiasKrarup <math249e@easv365.dk>
2022-03-20 03:14:24 +0100

    EditEventManager controller update

--- src/dk/easv/GUI/Controller/AdminViewController.java
+++ src/dk/easv/GUI/Controller/AdminViewController.java
@@ -1,5 +1,6 @@
 package dk.easv.GUI.Controller;
 
+import dk.easv.BE.EventManager;
 import dk.easv.BE.Person;
 import dk.easv.GUI.Model.PersonModel;
 import javafx.event.ActionEvent;
@@ -8,6 +9,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -55,15 +58,65 @@ public class AdminViewController extends SuperController implements Initializabl
         }
     }
 
-    public void handleOpenEdit(ActionEvent actionEvent) {
-        // FIXME: 3/19/2022
+    /**
+     * Opens the fxml scene where you can edit an eventmanager
+     *
+     * @param actionEvent
+     */
+
+    public void handleOpenEdit(ActionEvent actionEvent) throws IOException {
+        /*
+        EventManager selectedEventmanager = (EventManager) eventmanagerTable.getSelectionModel().getSelectedItem();
+        if (selectedEventmanager != null) {
+            FXMLLoader root = new FXMLLoader(getClass().getResource("/dk/easv/GUI/View2/EditEventManager.fxml"));
+            Scene mainWindowScene = new Scene(root.load());
+
+            Stage editEventmanagerStage = new Stage();
+            editEventmanagerStage.setScene(mainWindowScene);
+            EditEventmanagerController editEventmanagerController = root.getController();
+            editEventmanagerController.();
+            editEventmanagerStage.setResizable(true);
+            editEventmanagerStage.showAndWait();
+            eventmanagerTable.getItems().clear();
+            eventmanagerTable.setItems(personModel.getObservablePersons());
+        } else {
+            error("Select an eventmanager and try again");
+        }
+         */
+        // FIXME: 3/20/2022 
     }
 
+
+    /**
+     * Shows error message
+     * @param text
+     */
+
+    private void error(String text) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, text, ButtonType.OK);
+        alert.showAndWait();
+    }
+
+    /**
+     * Opens the fxml scene where you can add a new eventManager from
+     * @param actionEvent
+     * @throws IOException
+     */
     public void handleOpenAdd(ActionEvent actionEvent) throws IOException {
         openScene("/dk/easv/GUI/View2/AddEventManager.fxml", false, true,"Add Eventmanager", true);
         eventmanagerTable.getItems().clear();
         eventmanagerTable.setItems(personModel.getObservablePersons());
     }
+
+    /**
+     * Method used to open a new fxml scene
+     * @param pathToFXML
+     * @param undecorated
+     * @param showAndWait
+     * @param title
+     * @param resizable
+     * @throws IOException
+     */
 
     private void openScene(String pathToFXML, boolean undecorated, boolean showAndWait, String title, boolean resizable) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource(pathToFXML));
--- src/dk/easv/GUI/Controller/EditEventmanagerController.java
+++ src/dk/easv/GUI/Controller/EditEventmanagerController.java
@@ -1,16 +1,43 @@
 package dk.easv.GUI.Controller;
 
+import dk.easv.BE.EventManager;
+import dk.easv.BE.Person;
+import dk.easv.GUI.Model.PersonModel;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class EditEventmanagerController {
+public class EditEventmanagerController extends SuperController {
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private TextField passwordTxtfield;
+    @FXML
     private TextField emailTxtField;
+    @FXML
     private TextField nameTxtField;
+    @FXML
     private TextField eventTxtField;
 
+    private PersonModel personModel;
+
     public void handleSaveBtn(ActionEvent actionEvent) {
+        // FIXME: 3/20/2022 
     }
 
     public void handleCancelBtn(ActionEvent actionEvent) {
+        closeWindow(cancelBtn);
+    }
+
+    public void setEventmanager(EventManager eventmanager) {
+        // FIXME: 3/20/2022 
+    }
+
+    @Override
+    void setPersonInfo(Person person) {
+        // FIXME: 3/20/2022
     }
 }
