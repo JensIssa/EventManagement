543ea4d6e416929196b5dd40b0484bb2308aee4d
MathiasKrarupMadsen <Math249e@easv365.dk>
2022-03-21 11:18:30 +0100

    fxml update

--- src/dk/easv/GUI/Controller/EventManagerViewController.java
+++ src/dk/easv/GUI/Controller/EventManagerViewController.java
@@ -20,4 +20,7 @@ public class EventManagerViewController extends SuperController {
 
     public void handleAddUserToEvent(ActionEvent actionEvent) {
     }
+
+    public void handleAddEvent(ActionEvent actionEvent) {
+    }
 }
