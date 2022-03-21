9a173c49c4818f6e386cb3c9e50e7314dd0a0614
MathiasKrarupMadsen <Math249e@easv365.dk>
2022-03-21 10:43:37 +0100

    EditGuestView + AddGuestView

---
+++ src/dk/easv/GUI/Controller/EditGuestController.java
@@ -0,0 +1,20 @@
+package dk.easv.GUI.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditGuestController {
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private TextField passwordTxtField;
+    @FXML
+    private TextField phonenumberTxtField;
+    @FXML
+    private TextField emailTxtField;
+    @FXML
+    private TextField nameTxtField;
+}
