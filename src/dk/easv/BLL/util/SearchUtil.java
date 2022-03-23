31fb6a8b6387605952429d0fce349c828c5e620f
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-03-23 19:41:22 +0100

    filled the search class up with a working method, REMEMBER TO DO THIS LATER

--- src/dk/easv/BLL/util/SearchUtil.java
+++ src/dk/easv/BLL/util/SearchUtil.java
@@ -1,4 +1,24 @@
 package dk.easv.BLL.util;
 
+import dk.easv.BE.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchUtil {
+
+    public List<User> search(List<User> searchBase, String query){
+        List<User> searchResult = new ArrayList<>();
+
+        for (User user: searchBase){
+            if (compareToName(query, user)) {
+            searchResult.add(user);
+            }
+            }
+        return searchResult;
+        }
+
+    private boolean compareToName(String query, User user){
+        return user.getName().toLowerCase().contains(query.toLowerCase());
+    }
 }
