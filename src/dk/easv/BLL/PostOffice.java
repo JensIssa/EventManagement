035739ef862f0c87c57e84daaf3fa11f19a1a87e
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-04-03 22:19:00 +0200

    Postoffice rework

--- src/dk/easv/BLL/PostOffice.java
+++ src/dk/easv/BLL/PostOffice.java
@@ -2,84 +2,80 @@ package dk.easv.BLL;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javax.sql.DataSource;
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URLEncoder;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PostOffice {
+    private String getOutlook() {
+        try {
+            Process p = Runtime.getRuntime()
+                    .exec(new String[]{"cmd.exe", "/c", "assoc", ".pst"});
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String extensionType = input.readLine();
+            input.close();
+            // extract type
+            if (extensionType == null) {
+                outlookNotFoundMessage("File type PST not associated with Outlook.");
+            } else {
+                String[] fileType = extensionType.split("=");
 
-    private static final String PROP_FILE = "src/emailInfo.txt";
+                p = Runtime.getRuntime().exec(
+                        new String[]{"cmd.exe", "/c", "ftype", fileType[1]});
+                input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String fileAssociation = input.readLine();
+                // extract path
+                Pattern pattern = Pattern.compile("\".*?\"");
+                Matcher m = pattern.matcher(fileAssociation);
+                if (m.find()) {
+                    String outlookPath = m.group(0);
+                    System.out.println(outlookPath);
+                    return outlookPath;
+                } else {
+                    outlookNotFoundMessage("Error parsing PST file association");
+                }
+            }
 
-
-    String userEmail;
-    String userPassword;
-    Session newSession = null;
-    MimeMessage mimeMessage = null;
-
-    public PostOffice() throws IOException {
-        readPropfile();
-    }
-
-    private void readPropfile() throws IOException {
-        Properties loginProp = new Properties();
-        loginProp.load(new FileInputStream(PROP_FILE));
-        userEmail = loginProp.getProperty("Email");
-        userPassword = loginProp.getProperty("Password");
-    }
-
-    //Setup mail serverProperties
-    private void setupServerProperties() {
-        Properties properties = System.getProperties();
-        properties.put("mail.smtp.port", 587);
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        newSession = Session.getDefaultInstance(properties, null);
-        //Session var ikke sat i tut, måske en fejl
-    }
-//draft mail
-
-    private MimeMessage draftEmail() throws MessagingException {
-        String[] mailingList = {"vict657k@easv365.dk"}; //email list skal være en String array, måske??
-        String subject = "subject placeholder";
-        String body = "body placeholder";
-        mimeMessage = new MimeMessage(newSession);
-
-        for (String emailAddress : mailingList) {
-            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));
+        } catch (Exception err) {
+            err.printStackTrace();
+            outlookNotFoundMessage(err.getMessage());
         }
-        mimeMessage.setSubject(subject);
-
-        MimeBodyPart bodyPart = new MimeBodyPart();
-        bodyPart.setContent(body, "html/text");
-
-        MimeMultipart multipart = new MimeMultipart();
-        multipart.addBodyPart(bodyPart);
-
-        mimeMessage.setContent(multipart);
-        return mimeMessage;
+        return null;
     }
 
-    //send mail
-    private void sendEmail() throws MessagingException {
-        //replace med en proterty fil i stil med databse connector
-        String fromUser = userEmail; //senderen skal være en gmail
-        String fromUserPassword = userPassword;
-        String emailHost = "smtp.gmail.com";
-        Transport transport = newSession.getTransport("smtp");
-        transport.connect(emailHost,fromUser,fromUserPassword);
-        transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
-        transport.close();
-        System.out.println("mail sendt");
+    private static void outlookNotFoundMessage(String errorMessage) {
+        System.out.println("Could not find Outlook: \n" + errorMessage);
     }
 
-    public static void main(String[] args) throws MessagingException, IOException {
+    public void openOutlook() {
+        String outlook = getOutlook();
+        Runtime rt = Runtime.getRuntime();
+        //C:\Users\deaso>"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /m "cchesberg@gmail.com" /c ipm.note /a "c:\Users\deaso\random.dat
+
+        try {
+            String attachment = "src/Passivelapwing_9f0fdb_8839861.jpg";
+            //String attachment = "resources/TempTickets/"+ticketSold.getTicketNumber()+".png";
+            File file = new File(attachment);
+            System.out.println(file.getAbsolutePath());
+            rt.exec(new String[]{"cmd.exe", "/c", outlook, "/m", "vict657k@easv365.dk?subject=Ticket_Email", "/a", file.getAbsolutePath()});
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
         PostOffice postOffice = new PostOffice();
-        postOffice.setupServerProperties();
-        postOffice.draftEmail();
-        postOffice.sendEmail();
+        postOffice.openOutlook();
     }
-
 }
